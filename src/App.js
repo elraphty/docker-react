72d458475e3775b354953b3879c70c0239a6ec4c
elraphty <elraphtyofficial@gmail.com>
2021-09-14 11:28:58 +0100

    slight change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Eyerin
+          Learn Raphael Eyerin
         </a>
       </header>
     </div>
